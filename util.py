9ba8c3cd84783c055dec33dfc15ddcf0095c2bba
Weian Cheng <weian_cheng@htc.com>
2018-07-31 12:02:33 +0800

    add functions for calculating hash code.

---
+++ util.py
@@ -0,0 +1,39 @@
+import re
+
+
+def parse_int(sin):
+    m = re.search(r'^(\d+)[.,]?\d*?', str(sin))
+    return int(m.groups()[-1]) if m and not callable(sin) else None
+
+
+def nn(n):
+    if n < 10:
+        return '00' + str(n)
+    else:
+        if n < 100:
+            return '0' + str(n)
+        else:
+            return str(n)
+
+
+def mm(p):
+    return (parse_int((p - 1) / 10) % 10) + (((p - 1) % 10) * 3)
+
+
+def su(a, b, c):
+    return a[b:b+c]
+
+
+def lc(l):
+    if len(l) != 2:
+        return l
+
+    az = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    a = l[0:1]
+    b = l[1:2]
+
+    if a == 'Z':
+        return str(8000 + az.index(b))
+    else:
+        return str(az.index(a) * 52 + az.index(b))
