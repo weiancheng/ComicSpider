4a58411d55ece5d53bcda8e3c5609cdeecf78de7
Weian Cheng <weian_cheng@htc.com>
2018-08-15 17:50:38 +0800

    add PageMenu for manage pages of an episode of a comic book.

---
+++ UI/Viewer/PageMenu.py
@@ -0,0 +1,13 @@
+from PyQt5.QWidgets import QListWidget
+
+
+class PageMenu(QListWidget):
+    def __init__(self):
+        super().__init__()
+
+    def addItem(self, index, string):
+        pass
+
+    def currentItem(self):
+        pass
+
