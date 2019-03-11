bd0c52b2d64bdcbd5caf819522d451566106a7d9
Weian <d92434101@gmail.com>
2019-03-12 07:23:39 +0800

    complete the manhuagui crawler

--- manga.py
+++ manga.py
@@ -1,30 +1,21 @@
 from manhuagui.crawl_manhuagui import crawl_manhuagui
 from manhuagui.episode import episode
 from manhuagui.comic_book import comic_book
-from manhuagui.constant import MANHUAGUI_COMIC_URL
 
 
-class ComicAPI:
-    def __init__(self):
-        pass
+def comic_book_list(start=0, end=0):
+    result_list = list()
 
-    def get_list(self):
-        yield crawl_manhuagui()
+    for url in crawl_manhuagui(start, end):
+        print(url)
+        result_list.append(url)
 
-    def get_comic_book(self, index):
-        url = MANHUAGUI_COMIC_URL + str(index)
-        data = comic_book(url)
-        return data
-
-    def get_content(self, url):
-        return episode(url)
+    return result_list
 
 
-def main():
-    comic = ComicAPI()
-    print(comic.get_comic_book('https://www.manhuagui.com/comic/7580'))
-    print(comic.get_content('https://www.manhuagui.com/comic/3/64331.html'))
+def comic_book_album(url):
+    return comic_book(url)
 
 
-if __name__ == '__main__':
-    main()
+def comic_book_chapter(url):
+    return episode(url)
