c4dbae46b96a8299d26aa3f1ee8a1b21a603c3f9
Weian <d92434101@gmail.com>
2019-10-05 22:52:06 +0800

    return session

--- manhuagui/crawl_manhuagui.py
+++ manhuagui/crawl_manhuagui.py
@@ -2,17 +2,27 @@ import requests
 from manhuagui.constant import MANHUAGUI_COMIC_URL
 
 
-def crawl_manhuagui(start=0, end=0):
+def crawl_manhuagui(session=None, start=1, end=0):
     index = start
 
+    output = list()
+
+    manhuagui_session = session
+    if not manhuagui_session:
+        manhuagui_session = requests.session()
+
+    output.append(manhuagui_session)
+
     while True:
-        response = requests.get(MANHUAGUI_COMIC_URL + str(index))
+        response = manhuagui_session.get(MANHUAGUI_COMIC_URL + str(index))
         if response.status_code != requests.codes.ok:
             break
 
-        yield MANHUAGUI_COMIC_URL + str(index)
+        output.append(MANHUAGUI_COMIC_URL + str(index))
 
         if index == end and end != 0:
             break
 
         index += 1
+
+    return output
